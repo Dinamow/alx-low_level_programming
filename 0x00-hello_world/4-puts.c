123538c5ad6742ccf903d286148c1e79891f3a28
Dinamow <meemoo102039@gmail.com>
2023-03-17 00:13:42 +0200

    5th

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	char str[100];
-	strcpy(str, "Programming is like building a multilingual puzzle");
-	puts(str);
+	strcpy (str, "\"Programming is like building a multilingual puzzle");
+	puts (str);
 	return 0;
 }
