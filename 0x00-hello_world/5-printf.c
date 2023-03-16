30943a97615f07970ed1fbab34b6f7fd7cd61626
Dinamow <meemoo102039@gmail.com>
2023-03-17 01:08:09 +0200

    6th

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - start of the func
  * printing a string
- * end and return 0
+ * end and return value 0
 */
 int main(void)
 {
     /*printing*/
 printf("with proper grammar, but the outcome is a piece of art,\n");
-    /*end*/
+    /*ending*/
 return (0);
 }
