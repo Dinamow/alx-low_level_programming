b230bedfc73cea28e422e4068cb458a01e8d0d46
Dinamow <meemoo102039@gmail.com>
2023-03-17 00:46:22 +0200

    5th

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 /* main.c */
 #include <stdio.h>
 #include <string.h>
-
-int main(void)
 /*
-*idk
+*main - start
+*allocate space for string
+*type the string 
 */
+int main(void)
 {
 	char str[100];
 		strcpy(str, "\"Programming is like building a multilingual puzzle");
