a51c28da5dc6748a714fa10f471aebb18226493f
Dinamow <meemoo102039@gmail.com>
2023-03-17 01:02:08 +0200

    6th

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 /*main*/
 #include <stdio.h>
 
-    /*main func*/int main(void)
+    /*main func*/
+int main(void)
 {
     /*printing*/
 printf("with proper grammar, but the outcome is a piece of art,\n");
