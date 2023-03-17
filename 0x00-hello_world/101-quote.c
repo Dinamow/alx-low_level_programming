e2d67a6deb1b73894092a2680d23d6345e597452
Dinamow <meemoo102039@gmail.com>
2023-03-17 03:47:55 +0200

    7th

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - start point
+ * Return: 1 (success)
+*/
+int main(void)
+{
+    fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+    return (1);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+/*main*/
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * 
+*/
+int main(char *argv[])
+{
+    if (strcmp(argv, "size32") == 0)
+    {
+        printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n");
+    }
+    else if (strcmp(argv, "size64"))
+    {
+        printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n");
+    }
+    
+}
