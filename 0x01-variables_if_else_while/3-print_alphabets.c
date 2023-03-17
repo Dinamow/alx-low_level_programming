3944531f9e3b3c5302fabafc39119f8055765b0b
Dinamow <meemoo102039@gmail.com>
2023-03-18 00:08:20 +0200

    5th

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - start point
+ * some stuff
+ * Return: 0 (success)
+*/
+int main(void)
+{
+char char1 = 'a';
+char char2 = '\n';
+char char3 = 'A';
+for (char1 = 'a'; char1 <= 'z'; char1++)
+putchar(char1);
+for (char3 = 'A'; char3 <= 'Z'; char3++)
+putchar(char3);
+putchar(char2);
+return (0);
+}
