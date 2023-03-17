73da571f2ae4cbe65842b40e124d9b4ef309489c
Dinamow <meemoo102039@gmail.com>
2023-03-18 00:29:32 +0200

    idk

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - start point
+ * some stuff
+ * Return: 0 (success)
+*/
+int main(void)
+{
+char char1 = '1';
+char char2 = '\n';
+for (char1 = '1'; char1 <= '9'; char1++)
+putchar(char1);
+putchar(char2);
+return (0);
+}
