f7fdf20bfe3dd7d2c9ceb4a9ebf7df0278a53764
Dinamow <meemoo102039@gmail.com>
2023-03-18 00:37:11 +0200

    sa

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - start point
+ * some stuff
+ * Return: 0 (success)
+*/
+int main(void)
+{
+char char1 = 'z';
+char char2 = '\n';
+for (char1 = 'z'; char1 >= 'a'; char1--)
+putchar(char1);
+putchar(char2);
+return (0);
+}
