a63abdcc58987c97aa2e1107c7b080b138f5cbfa
Dinamow <meemoo102039@gmail.com>
2023-03-17 23:23:59 +0200

    3rd

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,128 +1,23 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
- * main - start point
- * get n value
- * doing some math to get answer
- * Return: 0 (success)
+ * main- print the last digit and compare
+ * Return: 0 if the last digit is zero and number otherwise 
  */
 int main(void)
 {
-     int n;
-     int lenth;
-     int ten = 10;
-     int numbers = 1;
-     int copy = 0;
-     int lol = -1;
-     int answer;
-     int q;
-     int nums[1000];
-     int s;
-     int repeat;
+	int n;
+	int last_digit;
 
-     srand(time(0));
-     n = rand() - RAND_MAX / 2;
-     s = n;
-     q = s;
-     if (n > 0)
-     {
-          for(lenth = 0; s >= 1; lenth++)
-               s = n / ten;
-               ten = ten * 10;
-          
-          repeat = lenth;
-          for (int i = 0; i < lenth; i++)
-               s = n;
-               for (int j = 0; j < repeat - 1; j++)
-                    numbers = numbers * 10;
-               if (i == 0)
-               {
-                    for (int j = 0; s > 1; j++)
-                         int s1 = s;
-                         s = s / (numbers * (j + 1));
-                         if (s == 0)
-                         {
-                              nums[i] = s1 * numbers;
-                         }
-               }
-               else
-               {
-                    s = q;
-                    int lol = 1;
-                    for (int j = 0; lol >= 1; j++)
-                         lol = s / (nums[i - 1] + (numbers * (j + 1)));
-                         if(s / (nums[i - 1] + (numbers * (j + 1))) == 0)
-                         {
-                              if (repeat == 1)
-                              {
-                                   nums[i] = j;
-                              }
-                              nums[i] = (nums[i - 1] + (j * numbers));
-                         }                  
-               }
-               repeat = repeat - 1;
-               numbers = 1;
-               answer = nums[i];
-     }
-     else if (n < 0)
-     {
-          for(lenth = 0; s <= -1; lenth++)
-               s = n / ten;
-               ten = ten * 10;
-
-          int repeat = lenth;
-          int nums[lenth];
-          for (int i = 0; i < lenth; i++)
-               s = n;
-               for (int j = 0; j < repeat - 1; j++)
-                    numbers = numbers * 10;
-
-               if (i == 0)
-               {
-                    for (int j = 0; s <= -1; j++)
-                         int s1 = s;                        
-                         s = s / (numbers * (j + 1));
-                         if (s == 0)
-                         {
-                              nums[i] = s1 * numbers;
-                         }
-               }
-               else
-               {
-                    int lol = -1;
-                    s = q;
-                    for (int j = 0; lol <= -1; j++)
-                         lol = s / (-nums[i - 1] + (numbers * (j + 1)));
-                         if (s / (-nums[i - 1] + (numbers * (j + 1))) == 0)
-                         {
-                              if (repeat == 1)
-                              {
-                                 nums[i] = -j;
-                                 break;  
-                              }
-                              
-                              nums[i] = (nums[i - 1] + -(j * numbers));
-                         }
-                                                                   
-               }
-               repeat = repeat - 1;
-               numbers = 1;
-               answer = nums[i];
-          }
-
-     }
-     if (answer > 5)
-     {
-          printf("last digit of %d is %d and is greater than 5\n",q ,answer );
-     }
-     else if (answer > 0)
-     {
-          printf("last digit of %d is %d and is 0\n",q ,answer );
-     }
-     else if (answer < 6  && answer != 0)
-     {
-          printf("last digit of %d is %d and is less than 6 and not 0\n",q ,answer );
-     }
-     return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+        if (last_digit > 5)
+	  printf("Last digit of %d is %d and is grater than 5\n", n, last_digit\n);
+	else if (last_digit < 6 && last_digit != 0)
+	  printf("Last digit of %d is %d and less than 6 and not 0\n", n, last_digit);
+	else if (last_digit == 0)
+	  printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	return (0);
 }
