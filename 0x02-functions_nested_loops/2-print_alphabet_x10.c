40f0e147e6350369d68103efe3b17320ac131683
Dinamow <meemoo102039@gmail.com>
2023-03-22 00:32:19 +0200

    3rd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,11 @@
 void print_alphabet_x10(void)
 {
     for(int i = 0; i < 10; i++)
-    {
+    
         char c;
         for (c = 'a'; c <= 'z'; c++)
         _putchar(c);
 
         _putchar('\n');
-    }
+    
 }
