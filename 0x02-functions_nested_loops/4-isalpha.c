36a1dfd71e1dd5e1f1684a57b3ff095d5419386a
Dinamow <meemoo102039@gmail.com>
2023-03-22 00:54:13 +0200

    1st

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _isalpha - start point 
+ * take parameter c 
+ * Return: 1 (lowercase or uppercase) , 0 (else)
+*/
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
