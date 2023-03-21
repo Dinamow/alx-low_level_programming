e854cb59602b13948764b37f293cab4bb91b7fc9
Dinamow <meemoo102039@gmail.com>
2023-03-22 00:36:04 +0200

    s

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,19 @@
 #include "main.h"
 #include <unistd.h>
-
+/**
+ * print_alphabet_x10 - starting point
+ * print alphabet
+*/
 void print_alphabet_x10(void)
 {
-    int i = 0;
-    while (i < 10)
-    {
-        char c;
-        for (c = 'a'; c <= 'z'; c++)
-        _putchar(c);
+int i = 0;
+while (i < 10)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
 
-        _putchar('\n');
-        i++;
-    }
+_putchar('\n');
+i++;
+}
 }
