6e00a7775992132d1d11616e8563b6088e90e44c
Dinamow <meemoo102039@gmail.com>
2023-03-22 01:51:56 +0200

    test

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,6 +3,18 @@
 
 int print_last_digit(int c);
 {
-int i = c % 10;
-return (i);
+	int digit;
+
+	digit = num % 10;
+
+	if (digit < 0)
+	{
+		_putchar(-digit + '0');
+		return (-digit);
+	}
+	else
+	{
+		_putchar(digit + '0');
+		return (digit);
+	}
 }
