c1950ad0c7e19f8389920e43e37fb639d9232324
Dinamow <meemoo102039@gmail.com>
2023-03-22 00:48:11 +0200

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,8 @@
 #include <unistd.h>
 /**
  * _islower - start point
- * check the input c
- * return: 1 (lower)
- * return: 0 (else)
+ * take int c
+ * return: 1 (lower), 0 (else)
 */
 int _islower(int c)
 {
