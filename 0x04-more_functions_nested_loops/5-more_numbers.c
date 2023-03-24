f673d6ddfacdc30598c862bcaf5846a388fe9061
Dinamow <meemoo102039@gmail.com>
2023-03-24 04:15:09 +0200

    test

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,8 @@ void more_numbers(void)
 int i, j;
 for(i = 0; i < 9; i++)
 {
-    for(j = 0; j < 15; j++)
-    _putchar(j + 48);
+    for(j = '0'; j < '15'; j++)
+    _putchar(j);
 _putchar('\n');
 }
 
