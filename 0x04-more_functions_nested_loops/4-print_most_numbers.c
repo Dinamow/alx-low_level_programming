d148289a693f77a3bcb3bc579acbe179f069631b
Dinamow <meemoo102039@gmail.com>
2023-03-24 04:01:16 +0200

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,11 +5,11 @@ void print_most_numbers(void)
 int c = '0';
 do
 {
-if (c != '2' && c != '4')
-{
-_putchar(c);
-}
-c++;
+    if (c != '2' && c != '4')
+    {
+        _putchar(c);
+    }
+    c++;
 } while (c <= '9');
 _putchar('\n');
 }
