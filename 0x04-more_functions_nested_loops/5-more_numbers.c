2fd3d163c2d836fec55f4c7ba915e812e6d833a2
Dinamow <meemoo102039@gmail.com>
2023-03-24 04:12:52 +0200

    1st

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+ /**
+  * more_numbers - start point
+  * 2 loops
+  * print
+  * Return always 0(success)
+ */
+
+void more_numbers(void)
+{
+int i, j;
+for(i = 0; i < 9; i++)
+    for(j = 0; j < 15; j++)
+    _putchar(j + 48);
+_putchar('\n');
+
+}
