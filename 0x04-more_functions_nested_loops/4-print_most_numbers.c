fa561210aece94664b06bb8ffd92174c5b69c060
Dinamow <meemoo102039@gmail.com>
2023-03-24 04:07:13 +0200

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * print_most_numbers - start point 
+ * get value called c
+ * print a;; numbers except 2,4
+ * Return always 0 (success)
+*/
 void print_most_numbers(void)
 {
 int c = '0';
