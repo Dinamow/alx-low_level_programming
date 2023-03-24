a12d692a43e3a12c4fedab28e3a8025c530c2764
Dinamow <meemoo102039@gmail.com>
2023-03-24 04:17:46 +0200

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,10 +9,15 @@
 void more_numbers(void)
 {
 int i, j;
-for(i = 0; i < 9; i++)
+i = 0;
+do
 {
     for(j = 0; j < 15; j++)
     _putchar(j + 48);
+i++
+} while (i < 10);
+
+    
 _putchar('\n');
 }
 
