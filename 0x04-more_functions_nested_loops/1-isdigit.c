4b3d6bf5d69dd7f2751a9c759b9c4b1bd70e93fd
Dinamow <meemoo102039@gmail.com>
2023-03-24 03:09:43 +0200

    1st

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - start point
+ * 
+ * @c: input
+ * 
+ * Return: 1 (digit), 0(else)
+*/
+
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+    return (1);
+}
+else
+{
+    return(0);
+}
+
+}
