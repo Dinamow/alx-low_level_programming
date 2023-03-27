14757df55f5c2ec776d48da0185efac94b33987b
Dinamow <meemoo102039@gmail.com>
2023-03-28 01:29:21 +0200

    1st

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * swap_int - start point
+ * @a: take intger as input
+ * @b: take another intger as input
+ * swap values
+ * Return always 0 (success)
+*/
+
+void swap_int(int *a, int *b)
+{
+int c;
+c = &a
+*a = &b
+*b = c
+}
