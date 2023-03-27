61e9c9c9d3396aa76a824473cd07eab08f102665
Dinamow <meemoo102039@gmail.com>
2023-03-28 01:16:57 +0200

    1st

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN.H
+#define MAIN.H
 
 #include <stdio.h>
 
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+#include <stdio.h>
+
+int _putchar(char name);
+void reset_to_98(int *n);
+
+#endif
