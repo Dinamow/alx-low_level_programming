b43534b3a0c6265469ed9dabb703cb91da88cf9d
Dinamow <meemoo102039@gmail.com>
2023-03-28 02:06:51 +0200

    1st

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - start point
+ * @str: take input
+ * print
+*/
+void _puts(char *str)
+{
+int i;
+for(i = 0; str[i] != '\0' ; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
