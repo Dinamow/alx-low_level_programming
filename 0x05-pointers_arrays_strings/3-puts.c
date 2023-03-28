046a21fe416191b682925f10761bd4f607b50a02
Dinamow <meemoo102039@gmail.com>
2023-03-28 02:07:21 +0200

    1st

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,11 +3,12 @@
  * _puts - start point
  * @str: take input
  * print
+ * Return always 0 (success)
 */
 void _puts(char *str)
 {
 int i;
-for(i = 0; str[i] != '\0' ; i++)
+for(i = 0; str[i] != '\0'; i++)
 _putchar(str[i]);
 _putchar('\n');
 }
