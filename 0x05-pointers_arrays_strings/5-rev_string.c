db1220b959fecd492c3f4a5f918c13810243c214
Dinamow <meemoo102039@gmail.com>
2023-03-28 02:30:27 +0200

    1st

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * rev_string - start point
+ * @s: input
+ * Return always 0 (success)
+*/
+void rev_string(char *s)
+{
+int i;
+int chars;
+char* s0;
+for (i = 0; s[i] != '\0'; i++)
+chars = i;
+for (i = 0; chars != -1; chars--)
+s0[i] = s[chars];
+for (i = 0; i <= chars; i++)
+s[i] = s0[i]
+}
