e6f8ec420110b601579cb977752eac9f65ada481
Dinamow <meemoo102039@gmail.com>
2023-03-28 02:14:05 +0200

    test

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,17 @@
 #include "main.h"
-
+/**
+ * print_rev - start point
+ * @s: take input
+ * rev chars
+ * print rev chars
+ * Return always 0 (sucess)
+*/
 void print_rev(char *s)
 {
 int i;
 int chars;
 for (i = 0; s[i] != '\0'; i++)
-chars = i
+chars = i;
 for (i = 0; chars != -1; chars--)
-_putchar('s[chars]')
+_putchar(s[chars]);
 }
