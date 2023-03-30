67051b4e1c0086173b078d8c573208297585bb8e
Dinamow <meemoo102039@gmail.com>
2023-03-30 23:52:35 +0200

    test

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * 
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+int equal = 0;
+
+while (*s1)
+{
+    if(*s1 != s2)
+    {
+        equal = ((int)*s1 - 48) - ((int)*s2 -48)
+    }
+    s1++;
+    s2++''
+}
+return (euqal);
+}
