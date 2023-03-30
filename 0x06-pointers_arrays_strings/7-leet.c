1bddf3ab9ced9068e6a09bb09374feeb889ac0d4
Dinamow <meemoo102039@gmail.com>
2023-03-31 00:41:39 +0200

    test

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * 
+*/
+
+char *leet(char *)
+{
+char *cp  c;
+char key[] = {'a', 'E', 'O', 'T', 'L'};
+int value = {4, 3, 0, 7 ,1};
+unsigned int i;
+while (8c)
+{
+    for (i = 0; i < sizeof(key) / sizeof(char); i++)
+    {
+        if(*c == key[i] || key[i] + 32)
+            *c = 84 + value[i];
+    }
+    c++;
+}
+return (cp);
+}
