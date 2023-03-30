7ee80f0c77df92a622ee2406f3df573468633571
Dinamow <meemoo102039@gmail.com>
2023-03-31 00:29:02 +0200

    t

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+int islower(char c)
+{
+    return (c >= 97 && c <= 122)
+}
+
+
+int isdelimiter(char c)
+{
+int i;
+char delimiter[] = " \t\n,.!?\"(){}"
+for (i = 0; i < 12; i++)
+{
+    if (c == delimiter[i])
+    retrun(1);
+}
+return (0);
+}
+
+/**
+ * cap_string - start point
+ * @char: input
+*/
+
+char *cap_string(char *)
+{
+char* ptr = s;
+int founddelimiter = 1;
+while (*s)
+{
+    if (isdelimiter(*s))
+    {
+        founddelimiter = 1;
+    }
+    else if (islower(*s) && founddelimiter)
+    {
+        *s -= 32;
+        founddelimiter = 1;
+    }
+    else
+    {
+        founddelimiter = 0;
+    }
+    s++;
+}
+return(ptr);
+}
