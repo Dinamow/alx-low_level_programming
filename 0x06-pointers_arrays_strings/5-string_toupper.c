b9e7ea7a3a27e9c79aaf97349a8656036c0073b4
Dinamow <meemoo102039@gmail.com>
2023-03-31 00:15:30 +0200

    tes

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - start point
+ * @char *: input
+ * Return: char*
+*/
+
+char *string_toupper(char *)
+{
+int i = 0;
+for (i = 0; str[i] != '\0'; i++)
+{
+    if (str[i] >= 97 && str[i] <= 122)
+    {
+        str[i] = str[i] - 32;
+    }
+}
+return (str);
+}
