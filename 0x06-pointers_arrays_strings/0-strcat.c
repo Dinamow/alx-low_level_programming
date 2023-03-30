89eec01fabcf6b86b521c2af8eb8fe441d0a4ede
Dinamow <meemoo102039@gmail.com>
2023-03-30 21:00:25 +0200

    tws

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,13 @@
 char *_strcat(char *dest, char *src)
 {
 int c, c2;
-
 c = 0;
-while (dest[c])
+while(dest[c])
 c++;
 
 for (c2 = 0; src[c]; c2++)
 dest[c++] = src[c2];
     
 return (dest);
+
 }
