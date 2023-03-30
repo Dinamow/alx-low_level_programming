4585e69b402b7366b66c0d01076e6bd43b6d7850
Dinamow <meemoo102039@gmail.com>
2023-03-30 23:54:47 +0200

    pew

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,13 @@ int _strcmp(char *s1, char *s2)
 {
 int equal = 0;
 
-while (*s1)
+while(*s1)
 {
     if (*s1 != s2)
-    {
-        equal = ((int)*s1 - 48) - ((int)*s2 -48)
-    }
+        equal = ((int)*s1 - 48) - ((int)*s2 -48);
+    
     s1++;
-    s2++''
+    s2++;
 }
 return (euqal);
 }
