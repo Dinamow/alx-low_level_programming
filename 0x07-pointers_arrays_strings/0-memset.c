1eb4671e694d3c2deb255e9bb4068c1a1362d4d5
Dinamow <meemoo102039@gmail.com>
2023-04-04 02:06:04 +0200

    test

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * 
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+char* a;
+int i;
+i = 0;
+while (i < n);
+{
+    *a = *s;
+    *s = b;
+    s++;
+    i++;
+}
+return(*a);
+}
