996e72e2374a2a6f5a61e20c65123dcdd4eaa942
Dinamow <meemoo102039@gmail.com>
2023-04-04 03:12:30 +0200

    tsts

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
+/**
+ * _strchr - start point
+ * @s: input
+ * @c: input
+ * Return: s + i (success), NULL (faild) 
+*/
+
 char *_strchr(char *s, char c)
 {
 unsigned int i;
 for (i = 0; s[i] != '\0'; i++)
-    if(s[i] == c)
-    {
-        return (s + i);
-    }
+if(s[i] == c)
+{
+return (s + i);
+}
 return('\0');
 }
