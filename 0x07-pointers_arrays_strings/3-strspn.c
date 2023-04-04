725d1160f77cf9c9af6f00302d1358f7fe5562db
Dinamow <meemoo102039@gmail.com>
2023-04-04 03:38:06 +0200

    tsts

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * _strspn - start point
  * @s: input
  * @accept: input
  * Return: i (success)
 */
-
 unsigned int _strspn(char *s, char *accept)
 {
     unsigned int i, j;
@@ -15,8 +13,8 @@ unsigned int _strspn(char *s, char *accept)
         for (j = 0; accept[j] != s[i]; j++)
         {
             if (accept[j] == '\0')
-                return (i);
+                return(i);
         }
     }
-    return (i);
+    return(i);
 }
