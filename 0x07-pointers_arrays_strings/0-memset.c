2b10d45a618640604a22c2fc13d42baf9aed15c3
Dinamow <meemoo102039@gmail.com>
2023-04-04 02:44:50 +0200

    ss

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,15 +2,19 @@
 #include <unistd.h>
 
 /**
- * 
+ * _memset - start point
+ * @s: input
+ * @b: input
+ * @n: input
+ * Return: s (success)
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int a;
-    for (a = 0; n > 0; a++, n--)
-    {
-        s[a] = b;
-    }
-    return(s);
+unsigned int a;
+for (a = 0; n > 0; a++, n--)
+{
+s[a] = b;
+}
+return(s);
 }
