47e7dab103337143718f72d5003caa0953ef3e33
Dinamow <meemoo102039@gmail.com>
2023-04-04 02:33:43 +0200

    test

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,15 +7,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-char* a;
-int i;
-i = 0;
-while (i < n);
-{
-    *a = *s;
-    *s = b;
-    s++;
-    i++;
-}
-return(a);
+    unsigned int a;
+    for (a = 0; n > 0; a++, n--)
+    {
+        s[a] = b;
+    }
+    return(s);
 }
