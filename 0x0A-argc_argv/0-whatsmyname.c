40565075573fb59977f443e192aa91ff0cd79784
Dinamow <meemoo102039@gmail.com>
2023-04-11 02:24:41 +0200

    tsts

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - start pint
+ * @argc: input
+ * @argv: input
+ * print
+ * Return: Always 0 (success)
+*/
 int main(int argc, char** argv)
 {
-    printf("%s\n", argv[argc - 1]);
-    return 0;
+printf("%s\n", argv[argc - 1]);
+return 0;
 }
