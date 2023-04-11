faec2e75058760ddf874ae2ed5c0a1c41e8fc865
Dinamow <meemoo102039@gmail.com>
2023-04-12 01:13:40 +0200

    s

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,20 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * create_array - start point
+ * @size: input
+ * @c: input
+ * allocate space
+ * give some values
+ * Return: NULL (failed),n (success)
+*/
 char *create_array(unsigned int size, char c)
 {
-    char* n = malloc(size);
-    if (size == 0 || n == NULL)
-    {
-        return(NULL);
-    }
-    else
-    {
-        while (size--)
-        {
-            n[size] = c;
-        }
-        return(n);
-    }
+char* n = malloc(size);
+if (size == 0 || n == NULL)
+{
+return(NULL);
+}
+else
+{
+while (size--)
+{
+n[size] = c;
+}
+return(n);
+}
     
 }
