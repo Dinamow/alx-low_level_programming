5a51cd68eca9cbbb58123cd740cc40d72d18321f
Dinamow <meemoo102039@gmail.com>
2023-04-18 02:04:01 +0200

    test

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_h
+
+/**
+ * struct dog - start
+ * @name: dog's name
+ * @owner: dog's owner
+ * @age: dog's age
+*/
+struct dog
+{
+    char *name;
+    char *owner;
+    int age;
+};
+
+#endif
