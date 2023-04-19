814ee1e7045d190c51fb6ddc304873a920948782
Dinamow <meemoo102039@gmail.com>
2023-04-20 00:44:49 +0200

    test

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * print_name - start point
  * @name: input
@@ -7,6 +8,6 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-if (name && f)
-f(name);
+    if (name && f)
+        f(name);
 }
