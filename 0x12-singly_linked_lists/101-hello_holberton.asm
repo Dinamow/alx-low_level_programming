b4bfe000b68e2e0b238da6d2b0057da980077486
Dinamow <meemoo102039@gmail.com>
2023-04-27 22:57:50 +0200

    fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,8 +2,8 @@ section     .txt
     extern  printf
     global  main
 main
-    mov     edi, msg
-    mov     eax, 0
+    mov         edi, msg
+    mov         eax, 0
     call    printf
 
 section     .data
