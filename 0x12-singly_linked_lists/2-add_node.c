a8e2326c8961579687b1701b00bd9430a448994d
Dinamow <meemoo102039@gmail.com>
2023-04-27 22:26:27 +0200

    fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
         return (NULL);
     if (str)
     {
-        new_head->str = strdub(str);
+        new_head->str = strdup(str);
         if (!new_head->str)
         {
             free(new_head);
