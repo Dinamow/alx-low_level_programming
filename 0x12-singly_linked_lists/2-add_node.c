f50e1ef580d94ab1615750f3bce6a663f105188f
Dinamow <meemoo102039@gmail.com>
2023-04-27 22:28:17 +0200

    first

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,21 +9,21 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_head = malloc(sizeof(list_t));
+list_t *new_head = malloc(sizeof(list_t));
 
-    if (!head || !new_head)
-        return (NULL);
-    if (str)
-    {
-        new_head->str = strdup(str);
-        if (!new_head->str)
-        {
-            free(new_head);
-            return (NULL);
-        }
-        new_head->len = _strlen(new_head->str);
-    }
-    new_head->next = *head;
-    *head = new_head;
-    return (new_head);
+if (!head || !new_head)
+return (NULL);
+if (str)
+{
+new_head->str = strdup(str);
+if (!new_head->str)
+{
+free(new_head);
+return (NULL);
+}
+new_head->len = _strlen(new_head->str);
+}
+new_head->next = *head;
+*head = new_head;
+return (new_head);
 }
