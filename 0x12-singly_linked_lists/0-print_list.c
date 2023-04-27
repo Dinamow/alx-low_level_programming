61b96aa54fe69dc70119b8bcdac6e857af4b5cb4
Dinamow <meemoo102039@gmail.com>
2023-04-27 21:44:09 +0200

    pls

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * _strlen - start point
+ * @s: input
+ * 
+ * Return: int
+*/
+
+int _strlen(char* s)
+{
+int i = 0;
+if (!s)
+return 0;
+while(*s++)
+i++;
+return (i);
+}
+
+/**
  * print_lists - start point
  * @h: input
  * 
