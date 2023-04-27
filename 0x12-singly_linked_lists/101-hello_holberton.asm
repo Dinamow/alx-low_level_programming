f880260ea3aab48fe71e6ad3419bdae8e4e335c2
Dinamow <meemoo102039@gmail.com>
2023-04-27 22:54:36 +0200

    wtf

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,10 @@
+section     .txt
+    extern  printf
+    global  main
+main
+    mov     edi, msg
+    mov     eax, 0
+    call    printf
+
+section     .data
+    msg db  'hello, holberton', 0xa, 0
