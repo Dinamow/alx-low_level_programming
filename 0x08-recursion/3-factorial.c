b783a45051e56ab95f270de7132391bb774e3af5
Dinamow <meemoo102039@gmail.com>
2023-04-30 01:52:33 +0300

    first

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*factorial - gets the factorial of aa number
+*@n : user input
+*
+* Return: factorial.
+*/
+int factorial(int n)
+{
+if (n == 1)
+return (1);
+else
+if (n < 1)
+return (-1);
+else
+{
+return (n * factorial(n - 1));
+}
+}
