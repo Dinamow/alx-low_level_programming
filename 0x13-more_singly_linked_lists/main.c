94ea1cc7108ea44d0146c9931419121381137d9b
Dinamow <meemoo102039@gmail.com>
2023-04-30 01:49:16 +0300

    test

--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -12,6 +12,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
 
 
 int main(void)
@@ -20,16 +21,40 @@ int main(void)
 
     head = NULL;
     add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
     print_listint(head);
-    free_listint(head);
-    head = NULL;
+    free_listint2(&head);
+    printf("%p\n", (void *)head);
     return (0);
+}
+
+
+void free_listint2(listint_t **head)
+{
+    listint_t *node, *next_node, *for_head;
+    if (!head)
+        return;
+    node = head;
+    for_head = &head;
+    while (node)
+    {
+        next_node = node->next;
+        free(node);
+        node = next_node;
+    }
+    *for_head = NULL;
 }
 
 void free_listint(listint_t *head)
 {
 listint_t *node, *next_node;
-
+head = NULL;
 if (!head)
     return;
 node = head;
