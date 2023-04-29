423c7bfbcfae59b83398234dbd11e88e5ea19766
Dinamow <meemoo102039@gmail.com>
2023-04-29 11:52:16 +0300

    first

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,5 +2,15 @@
 
 size_t listint_len(const listint_t *h)
 {
-    /*returns the number of elements*/
+    size_t i = 0;
+    if (!h)
+    {
+        return 0;
+    }
+    while (h)
+    {
+        i++;
+        h = h->next;
+    }
+    return (i);
 }
