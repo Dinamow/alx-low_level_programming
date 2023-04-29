c3762dfb16a491e31b03853cd10613c28b9177e6
Dinamow <meemoo102039@gmail.com>
2023-04-30 02:32:34 +0300

    test

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,22 +1,28 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * int_index - start point
- * @array: input
- * @size: input
- * @cmp: input
- * 
- * Return: the index
-*/
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
-    i = 0;
-    if (array && size && cmp)
-        while (i < size)
-        {
-            if (cmp(array[i]))
-                return (i);
-            i++;
-        }
-        return (-1);
+	int i;
+
+	if (array == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
 }
