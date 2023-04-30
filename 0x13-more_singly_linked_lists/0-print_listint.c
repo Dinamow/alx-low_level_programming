4a9f7bcf41ffa1190d6c70cfe15ebb6cb50b363c
Dinamow <meemoo102039@gmail.com>
2023-04-30 18:53:10 +0300

    test

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
 */
 size_t print_listint(const listint_t *h)
 {
-    size_t i = 0;
-    if (!h)
-        return 0;
-    while (h)
-    {
-        printf("%d\n", h->n);
-        i++;
-        h = h->next;
-    }
-    return (i);
+size_t i = 0;
+if (!h)
+    return 0;
+while (h)
+{
+    printf("%d\n", h->n);
+    i++;
+    h = h->next;
+}
+return (i);
 }
