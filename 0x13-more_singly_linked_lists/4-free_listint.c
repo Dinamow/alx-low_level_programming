e29b07eefa1a1faf41553699ecaba2f33ae3c193
Dinamow <meemoo102039@gmail.com>
2023-04-30 21:05:08 +0300

    s

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,15 +8,15 @@
 */
 void free_listint(listint_t *head)
 {
-    listint_t *node, *next_node;
+listint_t *node, *next_node;
 
-    if (!head)
-        return;
-    node = head;
-    while (node)
-    {
-        next_node = node->next;
-        free(node);
-        node = next_node;
-    }
+if (!head)
+return;
+node = head;
+while (node)
+{
+next_node = node->next;
+free(node);
+node = next_node;
+}
 }
