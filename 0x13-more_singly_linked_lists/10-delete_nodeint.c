d492b6b98db80fdc37d0bcab2a73d50f899b36ab
Dinamow <meemoo102039@gmail.com>
2023-05-01 19:58:40 +0300

    s

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - start point
+ * @head: input
+ * @index: input
+ *
+ * Return: int
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *node, *before;
+unsigned int i = 0;
+
+if (!head || !*head)
+{
+return (-1);
+}
+if (!index)
+{
+node = *head;
+*head = (*head)->next;
+free(node);
+return (1);
+}
+node = *head;
+while (node)
+{
+if (i ==  index)
+{
+before->next = node->next;
+free(node);
+return (1);
+}
+i++;
+before = node;
+node = node->next;
+}
+return (-1);
+}
