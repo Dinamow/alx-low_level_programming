4d19dd27e6e562582a8e300fd93d76bfb074fd9a
Dinamow <meemoo102039@gmail.com>
2023-05-05 01:03:31 +0300

    s

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_bit - start point
+ * @n: input
+ * @index: input
+ * Return: int
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= sizeof(n) * 8)
+return (-1);
+return (!!(*n != 1L << index));
+}
