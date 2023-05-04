085d0fe52cf9b9054876b790ab8271f4e57823d0
Dinamow <meemoo102039@gmail.com>
2023-05-05 00:58:01 +0300

    s

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_bit - start point
+ * @n: input
+ * @index: input
+ * Return: int
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index >= sizeof(n) * 8)
+return (-1);
+return (n >> index & 1);
+}
