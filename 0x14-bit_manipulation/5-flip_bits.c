decda3250e1c97244271128f5a5879af04fc5c03
Dinamow <meemoo102039@gmail.com>
2023-05-05 01:20:09 +0300

    s

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * flip_bits - start point
+ * @n: input
+ * @m: input
+ * Return: unsigned int
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int xor = n ^ m;
+unsigned int count = 0;
+while (xor)
+{
+    if (xor & 1ul)
+    count++;
+    xor = xor >> 1;
+}
+return (count);
+}
