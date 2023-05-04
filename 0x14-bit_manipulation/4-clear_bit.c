643818fc2131da6da153da853477fd57d476baec
Dinamow <meemoo102039@gmail.com>
2023-05-05 01:14:35 +0300

    s

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * clear_bit - start point
+ * @n: input
+ * @index: input
+ * Return: int
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= sizeof(n) * 8)
+return (-1);
+if (*a & 1L << index)
+*n ^= 1L << index;
+return (1);
+}
