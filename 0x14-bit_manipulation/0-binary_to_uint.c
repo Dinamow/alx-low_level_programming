01ae070eb08b9a29036d8f4149f97aa39239cabb
Dinamow <meemoo102039@gmail.com>
2023-05-04 21:49:52 +0300

    s

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,45 +7,14 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-    int count = 0;
-    int temp[100];
-    int arr[100];
-    unsigned int result = 0;
-    int i; 
-    int j;
-    int x = 0;
-    for (j = 0; j < 100; j++)
+    unsigned int num = 0;
+    if (!b)
+        return (0);
+    while (*b)
     {
-        arr[j] = 0;
-    }
-    for (j = 0; j < 100; j++)
-    {
-        temp[j] = 0;
-    }
-    for (count; *b; count++, b++)
-    {
-        if (*b == '0')
-            x = 0;
-        else
-            x = 1;
-        temp[count] = x;
-    }
-    b = b - count;
-    count --;
-    for (i = 0; *b; i++, b++, count--)
-    {
-        if (!*b || *b != '0' && *b != '1')
+        if (*b != '1' && *b != '0')
             return (0);
-        else if (*b == '0')
-            x = 0;
-        else
-            x = 1;
-        arr[i] = temp[count] * pow(2,i);
+        num = num * 2 + (*b++ - '0');
     }
-    x = 0;
-    for (x = 0; x <= i; x++)
-    {
-        result += arr[x];
-    }
-    return (result);
+    return (num);
 }
