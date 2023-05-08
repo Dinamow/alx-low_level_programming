ac8b22a1a85974be60f25950b657170c638920b2
Dinamow <meemoo102039@gmail.com>
2023-05-08 23:07:50 +0300

    s

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,24 +9,24 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	int length = 0, len = 0;
-	char *ptr;
+int file;
+int length = 0, len = 0;
+char *ptr;
 
-	if (filename == NULL)
-		return (-1);
+if (filename == NULL)
+return (-1);
 
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file == -1)
-		return (-1);
+file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (file == -1)
+return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0, ptr = text_content; *ptr; ptr++)
-			len++;
-		length = write(file, text_content, len);
-    }
-	if (close(file) == -1 || len != length)
-		return (-1);
-	return (1);
+if (text_content != NULL)
+{
+for (len = 0, ptr = text_content; *ptr; ptr++)
+len++;
+length = write(file, text_content, len);
+}
+if (close(file) == -1 || len != length)
+return (-1);
+return (1);
 }
