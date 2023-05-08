364314b70945bbc1cccd1b934615fde4e6ac0c03
Dinamow <meemoo102039@gmail.com>
2023-05-08 22:26:29 +0300

    s

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,40 +4,40 @@
  * read_textfile - start point
  * @filename: input
  * @letters: input
- * 
+ *
  * Return: ssize_t
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int file;
-int length, chars;
-char *buf;
+    int file;
+    int length, chars;
+    char *buf;
 
-if (filename == NULL || letters == 0)
-return (0);
-buf = malloc(sizeof(char) * (letters));
-if (buf == NULL)
-return (0);
+    if (filename == NULL || letters == 0)
+        return (0);
+    buf = malloc(sizeof(char) * (letters));
+    if (buf == NULL)
+        return (0);
 
-file = open(filename, O_RDONLY);
-if (file == -1)
-{
-free(buf);
-return (0);
-}
-length = read(file, buf, letters);
-if (length == -1)
-{
-free(buf);
-close(file);
-return (0);
-}
+    file = open(filename, O_RDONLY);
+    if (file == -1)
+    {
+        free(buf);
+        return (0);
+    }
+    length = read(file, buf, letters);
+    if (length == -1)
+    {
+        free(buf);
+        close(file);
+        return (0);
+    }
 
-chars = write(STDOUT_FILENO, buf, length);
+    chars = write(STDOUT_FILENO, buf, length);
 
-free(buf);
-close(file);
-if (chars != length)
-return (0);
-return (length);
+    free(buf);
+    close(file);
+    if (chars != length)
+        return (0);
+    return (length);
 }
