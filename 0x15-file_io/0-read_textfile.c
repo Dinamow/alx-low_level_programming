a4fd77479d288690c5ccb2dfcf186be4a7384a84
Dinamow <meemoo102039@gmail.com>
2023-05-09 19:10:11 +0300

    s

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,21 +9,22 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buf;
+char buf[1024];
 ssize_t fd;
 ssize_t w;
 ssize_t t;
+if (!filename || !letters)
+return (0);
 
 fd = open(filename, O_RDONLY);
 
 if (fd == -1)
 return (0);
 
-buf = malloc(sizeof(char) * letters);
 t = read(fd, buf, letters);
 w = write(STDOUT_FILENO, buf, t);
 
 free(buf);
 close(fd);
-return (w);
+return (t);
 }
