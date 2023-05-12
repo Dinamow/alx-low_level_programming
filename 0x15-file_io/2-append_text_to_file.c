c0d8164dffa8ec493f3b76e7e1a145c0df0decb1
Dinamow <meemoo102039@gmail.com>
2023-05-12 14:17:09 +0300

    s

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,20 +9,26 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-if (!filename)
-return (-1);
-if (!filename)
-return (1);
-FILE* ptr;
-ptr = fopen("filename", "a");
-if (!ptr)
-return (-1);
+	int i = 0, file;
 
-while (*text_content)
-{
-fprintf(ptr ,"%s", *text_content);
-text_content++;
-}
-fclose(ptr);
-return (1);
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
+	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
