5fd5ed46a21bc1436522a1d79f9d6fc90ddc710b
Dinamow <meemoo102039@gmail.com>
2023-05-12 02:46:01 -0700

    pls

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,24 +9,24 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char* buf;
-ssize_t fd;
-ssize_t w;
-ssize_t t;
-if (!filename || !letters)
-return (0);
+	char* buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+	if (!filename || !letters)
+		return (0);
 
-fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-if (fd == -1)
-return (0);
+	if (fd == -1)
+		return (0);
 
-buf = malloc(letters);
+	buf = malloc(letters);
 
-t = read(fd, buf, letters);
-w = write(STDOUT_FILENO, buf, t);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-free(buf);
-close(fd);
-return (w);
+	free(buf);
+	close(fd);
+	return (w);
 }
