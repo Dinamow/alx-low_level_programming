270acfa369e7eae4614dade77b8bd5cdd2d15e1f
DINAMOW <meemoo102039@gmail.com>
2023-06-15 14:37:27 +0000

    gaga

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,16 +1,23 @@
 #include "lists.h"
+/**
+ * add_dnodeint - start
+ * @head: input
+ * @n: input
+ *
+ * Return: head
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new, *temp;
 	temp = *head;
 	new = malloc(sizeof(dlistint_t));
 
-	if(new != NULL)
+	if (new != NULL)
 	{
 		new->prev = NULL;
 		new->n = n;
 		new->next = temp;
-		if(temp != NULL)
+		if (temp != NULL)
 			temp->prev = new;
 		*head = new;
 	}
