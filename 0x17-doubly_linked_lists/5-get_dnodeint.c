2b46e48fa2533bd56aed0c938470ab0a12581887
DINAMOW <meemoo102039@gmail.com>
2023-06-15 15:57:52 +0000

    ez

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - start point
+ * @head: input
+ * @index: input
+ *
+ * Return: pointer
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *new;
+
+	for (i = 0; i < index; i++)
+		head = head->next;
+	new = head;
+	return (new);
+}
+
