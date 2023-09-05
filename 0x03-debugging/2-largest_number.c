310414e8cdb42fc4fd49666d237c07b65308bfbc
Dinamow <meemoo102039@gmail.com>
2023-09-05 10:01:28 +0000

    fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,9 +11,10 @@
 int largest_number(int a, int b, int c)
 {
 	if (a > b && a > c)
-		return(a);
+		return (a);
 	else if (b > a && b > c)
-		return(b);
-	else
-		return(c);
+		return (b);
+	else if (c > a && c > b)
+		return (c);
+	return (a);
 }
